0c77d82825915ffb0c804ec9d5f98b48be9182a0
Jeff <jeff372001@gmail.com>
2026-06-05 11:29:14 +0630

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "**.supabase.co" },
+    ],
+  },
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  },
+};
+
+module.exports = nextConfig;
